337630028d4177c296a9658a5ed35182bbbee2bd
guilherme737-b <guilhermefernandes49@gmail.com>
2022-01-27 00:44:21 -0300

    fist commit

---
+++ src/Doutor.java
@@ -0,0 +1,18 @@
+public class Doutor {
+
+    private String nome;
+    private Integer idade;
+
+    public Doutor(String nome, Integer idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+}
